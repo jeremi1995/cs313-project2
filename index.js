5322e1f42575d480018aff86e151b747c7780fce
jeremi1995 <jeremibq@gmail.com>
2020-06-27 16:29:02 -0600

    First commit to github

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get("/", (req, res)=>{
+  console.log("receiving request for: " + req.url)
+  res.send("Hello Project2");
+});
+
+app.listen(PORT, ()=>{console.log("listening on port " + PORT);});
