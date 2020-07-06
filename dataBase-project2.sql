a646adf9ccc3300e4b917b68b8a3b82f18275630
jeremi1995 <jeremibq@gmail.com>
2020-07-06 02:01:46 -0600

    added log-in, log-out functionality

--- dataBase-project2.sql
+++ dataBase-project2.sql
@@ -1,3 +1,12 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    user_name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    date_of_birth DATE
+);
+
 CREATE TABLE story (
     id SERIAL PRIMARY KEY NOT NULL,
     story_date DATE NOT NULL,
@@ -5,7 +14,14 @@ CREATE TABLE story (
     story_description TEXT NOT NULL
 );
 
+--Insert data to story
 INSERT INTO story (story_date, story_name, story_description) VALUES ('2020-06-27', 'Start Project2 Database', 'Today I started my project2 database');
+
+--Insert data to users:
+INSERT INTO users (first_name, last_name, user_name, password, date_of_birth) VALUES ('Jeremy', 'Duong', 'jeremi1995', 'uljlo78u8k', '1995-09-19');
 
 GRANT SELECT, INSERT, UPDATE ON story TO clientuser1;
 GRANT USAGE, SELECT ON SEQUENCE story_id_seq TO clientuser1;
+
+GRANT SELECT, INSERT, UPDATE ON users TO clientuser1;
+GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO clientuser1;
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
+
+//Use the session module to handle the user's session
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+//Set the port the app will be listening at
 app.set("port", (process.env.PORT || 5000));
 
 //Use this for req.body (query)
@@ -21,8 +31,48 @@ const pool = new Pool({ connectionString: connectionString });
 //Control
 
 app.get("/", (req, res) => {
-  console.log("receiving request for: " + req.url)
-  res.render("pages/main");
+  console.log("receiving request for: " + req.url);
+  let params = {};
+  if (req.session.user_name) {
+    params = { loggedIn: true, first_name: req.session.first_name, last_name: req.session.last_name };
+  }
+  else {
+    params = { loggedIn: false, first_name: 'N/A', last_name: 'N/A' };
+  }
+
+  res.render("pages/home", params);
+});
+
+//This endpoint is to handle login, logout request
+app.post("/", (req, res) => {
+  console.log("receiving request for: " + req.url);
+
+  if (req.body.login) {
+    logIn(req, function (err, result) {
+      let params = {};
+
+      if (!err && result.rows.length != 0) {
+        req.session.user_name = result.rows[0].user_name;
+        req.session.first_name = result.rows[0].first_name;
+        req.session.last_name = result.rows[0].last_name;
+        req.session.date_of_birth = result.rows[0].date_of_birth;
+        params = { loggedIn: true, first_name: req.session.first_name, last_name: req.session.last_name }
+
+      }
+      else {
+        console.log(err);
+        params = { loggedIn: false, first_name: 'N/A', last_name: 'N/A', messageFromServer: 'Cannot find credential' };
+      }
+
+      res.render("pages/home", params);
+    });
+  }
+  if (req.body.logout) {
+    req.session.destroy();
+    let params = { loggedIn: false, first_name: 'N/A', last_name: 'N/A'};
+
+    res.render("pages/home", params);
+  }
 });
 
 app.post("/addStory", (req, res) => {
@@ -30,7 +80,7 @@ app.post("/addStory", (req, res) => {
 
   insertStoryIntoDB(req, function (err, result) {
     console.log(JSON.stringify(result));
-    
+
     let params = {};
 
     if (err) {
@@ -39,7 +89,7 @@ app.post("/addStory", (req, res) => {
 
     }
     else {
-      params = { message: "Add story successful!"};
+      params = { message: "Add story successful!" };
     }
     res.render("pages/message", params);
   });
@@ -53,31 +103,53 @@ app.post("/viewStories", (req, res) => {
 
     if (err) {
       console.log(err);
-      params = {message: "Error: unable to query from DB"};  
+      params = { message: "Error: unable to query from DB" };
     }
     else {
       let message = "Story found!";
       let story = result.rows[0];
 
-      params = {message: message, story: story };
+      params = { message: message, story: story };
     }
     res.render("pages/message", params);
   });
+});
+
+//This part is just to test the functionality of the session
+app.get("/justDisplayMessage", (req, res) => {
+  console.log("receiving request for: " + req.url);
+  if (!req.session.viewCount) {
+    req.session.viewCount = 1;
+  }
+  else {
+    req.session.viewCount += 1;
+  }
+  let params = { message: req.session.viewCount };
+  res.render("pages/message", params);
 });
 
 app.listen(app.get("port"), () => { console.log("listening on port " + app.get("port")); });
 
 //Model: Database interaction
 
+function logIn(req, callback) {
+  console.log("looking for username and password");
+  let user_name = req.body.user_name;
+  let password = req.body.password;
+  console.log(user_name);
+  console.log(password);
+  let sqlCMD = "SELECT * FROM users WHERE user_name=$1 AND password=$2"
+  let params = [user_name, password];
+
+  pool.query(sqlCMD, params, callback);
+
+}
+
 function insertStoryIntoDB(req, callback) {
   console.log("Inserting story into db...");
   let story_date = req.body.story_date;
   let story_name = req.body.story_name;
   let story_description = req.body.story_description;
-
-  console.log(story_date);
-  console.log(story_name);
-  console.log(story_description);
 
   let sqlCMD = "INSERT INTO story (story_date, story_name, story_description) VALUES ($1,$2,$3)"
   let params = [story_date, story_name, story_description];
